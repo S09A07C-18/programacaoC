6bf0d8263040c5b5ac1c2f1adf07f05bedb86a39
S09A07C-18 <sofiac162007@gmail.com>
2026-03-09 18:09:40 +0000

    variaveisconfirmado

--- variaveis.c
+++ variaveis.c
@@ -1,1 +1,67 @@
+# include <stdio.h>
 
+int main(){
+
+    int idade;
+    int ID = 1;
+    float altura = 1.40;
+    char letra = 'S';
+    char nome[20] = "Sofia";
+    double salario = 50.000;
+
+    idade= 18;
+
+    printf("As variavéis foram inicializadas.\n");
+
+
+
+}
+
+
+/*---Variável---
+
+-armazena um valor
+
+----Declarar uma variável---
+
+-Criar 
+
+-Sempre é tipo_variável nome_variável
+
+-ex: int idade
+
+-nome de uma variávels sempre deve começar com letra( Miaúscula ou minúscula)ou subliando(_)
+
+-em seguida possso usar números, letras e sublinhados
+
+-exs:
+
+char letra1
+
+int idade_velho
+
+int quanti_1
+
+
+-nunca começa com um número 
+
+- não devo colocar nomes reservados de C ( nomes de funções)como nomes de variáveis
+
+- variáveis são sensitivas ao caso, ex: Idade e idade são variáveis diferentes
+
+
+---Inicializar uma variável---
+
+- Atribuir valor
+
+-sempre devo fazer para não dar erro(ela assumir valor aleatório(lixo))
+
+- Posso fazer no ínicio:
+
+ex: linha 6 desse código
+
+- Ou no meio do bloco de código:
+
+ex: linha 12 
+
+*/
